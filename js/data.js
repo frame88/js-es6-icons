12a166bc98cb810e14fc41c1ddca7f5be03ffaab
frame88 <fleuzzi97@gmail.com>
2021-12-16 19:30:17 +0100

    css e js quasi finito

--- js/data.js
+++ js/data.js
@@ -126,3 +126,36 @@ let utenti = all.filter((element) => {
 	return element.type == "user";
 });
 
+
+
+for (let i = 0; i < all.length; i++) {
+	container.innerHTML += boxsize(all[i]);
+}
+
+filter.addEventListener('change', function () {
+	if (filter.value == 'all') {
+		container.innerHTML = '';
+		all.forEach((element) => {
+			container.innerHTML += boxsize(element);
+		});
+	}
+	else if (filter.value == 'animal') {
+		container.innerHTML = '';
+		animali.forEach((element) => {
+			container.innerHTML += boxsize(element);
+		});
+	}
+	else if (filter.value == 'vegetable') {
+		container.innerHTML = '';
+		verdure.forEach((element) => {
+			container.innerHTML += boxsize(element);
+		});
+	}
+	else if (filter.value == 'user') {
+		container.innerHTML = '';
+		utenti.forEach((element) => {
+			container.innerHTML += boxsize(element);
+		});
+	}
+});
+
